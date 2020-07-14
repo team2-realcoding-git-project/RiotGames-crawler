608aca7542af7504a25c636aa6b2412ac73c1965
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-14 18:29:03 +0900

    riotgames crawler project for realcoding team2 is initialized

---
+++ riotgames-crawler/src/main/java/org/ajou/realcodingteam2/riotgamescrawler/RiotgamesCrawlerApplication.java
@@ -0,0 +1,13 @@
+package org.ajou.realcodingteam2.riotgamescrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotgamesCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotgamesCrawlerApplication.class, args);
+	}
+
+}
---
+++ riotgames-crawler/src/test/java/org/ajou/realcodingteam2/riotgamescrawler/RiotgamesCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ajou.realcodingteam2.riotgamescrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiotgamesCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
