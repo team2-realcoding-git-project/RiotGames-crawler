4f7533f3700023c1f2594495bdd287e48a9ec539
yoosh199 <yoosh199@github.com>
2020-07-15 17:58:04 +0900

    add variable FinalGameInformation.java

--- riotgames-crawler/src/main/java/org/ajou/realcodingteam2/riotgamescrawler/domain/FinalGameInformation.java
+++ riotgames-crawler/src/main/java/org/ajou/realcodingteam2/riotgamescrawler/domain/FinalGameInformation.java
@@ -1,4 +1,17 @@
 package org.ajou.realcodingteam2.riotgamescrawler.domain;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
 public class FinalGameInformation {
+    @Id
+    private String summonerName;
+    private boolean win;
+    private int championId;
+    private int kills;
+    private int deaths;
+    private int assists;
+    private String tier;
+    private String rank;
 }
