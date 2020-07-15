b1baa3c39704152505296c25c33cee16633f8f25
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-15 16:33:22 +0900

    checking duplication in mongodb for step4 is updated

--- riotgames-crawler/src/main/java/org/ajou/realcodingteam2/riotgamescrawler/service/RiotGamesService.java
+++ riotgames-crawler/src/main/java/org/ajou/realcodingteam2/riotgamescrawler/service/RiotGamesService.java
@@ -1,19 +1,19 @@
 package org.ajou.realcodingteam2.riotgamescrawler.service;
 
-import org.ajou.realcodingteam2.riotgamescrawler.domain.Game;
-import org.ajou.realcodingteam2.riotgamescrawler.domain.GameDetail;
-import org.ajou.realcodingteam2.riotgamescrawler.domain.League;
-import org.ajou.realcodingteam2.riotgamescrawler.domain.Summoner;
-import org.ajou.realcodingteam2.riotgamescrawler.repository.RiotGamesRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
+        import org.ajou.realcodingteam2.riotgamescrawler.domain.Game;
+        import org.ajou.realcodingteam2.riotgamescrawler.domain.GameDetail;
+        import org.ajou.realcodingteam2.riotgamescrawler.domain.League;
+        import org.ajou.realcodingteam2.riotgamescrawler.domain.Summoner;
+        import org.ajou.realcodingteam2.riotgamescrawler.repository.RiotGamesRepository;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.data.mongodb.core.MongoTemplate;
+        import org.springframework.data.mongodb.core.query.Criteria;
+        import org.springframework.data.mongodb.core.query.Query;
+        import org.springframework.scheduling.annotation.EnableScheduling;
+        import org.springframework.scheduling.annotation.Scheduled;
+        import org.springframework.stereotype.Service;
 
-import javax.xml.ws.soap.Addressing;
+        import javax.xml.ws.soap.Addressing;
 
 @Service
 @EnableScheduling
