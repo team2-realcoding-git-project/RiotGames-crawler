9773394e79ed49d9237578b5f412ebeaea4d9c2c
baewoojin514 <dnwls514@ajou.ac.kr>
2020-07-17 16:58:54 +0900

    controller and service are merged into one

--- riotgames-crawler/src/main/java/org/ajou/realcodingteam2/riotgamescrawler/controller/RiotGamesController.java
+++ riotgames-crawler/src/main/java/org/ajou/realcodingteam2/riotgamescrawler/controller/RiotGamesController.java
@@ -14,20 +14,6 @@ public class RiotGamesController {
     @Autowired
     private RiotGamesService riotGamesService;
 
-    @GetMapping("/riotGamesInfo-service/{summonerName}")
-    public Summoner getSummonerInfo(@PathVariable String summonerName){
-        return riotGamesService.getSummonerInform(summonerName);
-
-    }
-    @GetMapping("/riotGamesInfo-league/{summonerName}")
-    public League getLeagueInfo(@PathVariable String summonerName){
-        return riotGamesService.getLeagueInform(summonerName);
-    }
-    @GetMapping("/riotGamesInfo-gameId/{summonerName}")
-    public Game getGameInfo(@PathVariable String summonerName){
-        return riotGamesService.getGameInfo(summonerName);
-
-    }
     @GetMapping("/riotGamesInfo-game/{summonerName}")
     public FinalGameInformation getGameDetailInfo(@RequestParam String summonerName){
         return riotGamesService.getFinalGameInformation(summonerName);
